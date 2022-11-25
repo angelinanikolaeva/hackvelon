068adae5b480066952dc402a1d72f5342ead8b08
svetlana.maliugina <svetlana.maliugina@akvelon.com>
2022-11-25 19:07:50 +0300

    Add home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.scss";
 import React from "react";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import {Chat} from "./components/Chat";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Chat } from "./components/Chat";
 import NavBar from "./components/Layout/NavBar";
 import Content from "./components/Layout/Content";
+import HomePage from "./components/HomePage/HomePage";
+import BotsList from "./components/HomePage/BotsList";
 
 export default function App() {
   return (
@@ -12,7 +14,8 @@ export default function App() {
       <Content>
         <Routes>
           <Route path="/chat" element={<Chat />} />
-          <Route path="/" element={<div>Home</div>} />
+          <Route path="/bots" element={<BotsList />} />
+          <Route path="/" element={<HomePage />} />
         </Routes>
       </Content>
     </Router>
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,1 +1,22 @@
-export const languages = ["en", "fr", "de", "es", "nl"];
+export const languages = [
+  { key: "en", label: "English" },
+  { key: "fr", label: "French" },
+  { key: "es", label: "Spanish" },
+  { key: "de", label: "German" },
+  { key: "nl", label: "Dutch" },
+];
+
+export const bots = [
+  {
+    photo: "/src/assets/Avatar.png",
+    name: "Maria",
+    personality:
+      "Maria is a 21 years old girl. She is very polite and nice to talk to. She is extremely nerdy and bombards everyone with questions just about everything.",
+  },
+  {
+    photo: "/src/assets/Avatar.png",
+    name: "George Washington",
+    personality:
+      "Here is a chat with George Washington, the first president of the United States.",
+  },
+];
