6c30944978433146b2c37a7704b266ee4120ba85
Angelina Nikolaeva <nikolaeva.dev@gmail.com>
2022-11-26 00:40:36 +0300

    fix: languages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,16 @@
 import {Link} from "react-router-dom";
 import BotAvatar from "./BotAvatar";
 import "./Header.scss";
+import {languages} from "../constants/constants";
+
 const Header = ({name, language}) => {
+  const findLang = languages.find((lg) => lg.key === language)?.label;
   return (
     <div className="header">
       <BotAvatar name={name} width={120} height={120} />
       <div className="info">
         <div className="botName">{name}</div>
-        <div className="language">Choosen language: {language}</div>
+        <div className="language">Chosen language: {findLang}</div>
       </div>
       <Link className="button" to={{pathname: "/"}}>
         {" "}
