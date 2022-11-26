f1a0b4ac49e329e7a2040dc07b2ccad1ff425f5b
Angelina Nikolaeva <nikolaeva.dev@gmail.com>
2022-11-26 09:10:59 +0300

    upd: router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.scss";
 import React from "react";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {HashRouter as Router, Routes, Route} from "react-router-dom";
 import {Chat} from "./components/Chat";
 // import NavBar from "./components/Layout/NavBar";
 import Content from "./components/Layout/Content";
